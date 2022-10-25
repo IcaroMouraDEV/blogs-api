d43fd87e0799d606517836687d96d244ce63cb70
Icaro Joel <icaromoura.programador@gmail.com>
2022-10-25 18:35:23 -0300

    EU SOU UMA MULA BURRA

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -4,10 +4,10 @@ require('dotenv').config();
 
 module.exports = async (req, res, next) => {
   try {
-    const token = req.header('Authorization');
-    
+    const { authorization: token } = req.headers;
+
     if (!token) {
-      return res.status(401).json({ error: 'Token not found' });
+      return res.status(401).json({ message: 'Token not found' });
     }
     
     const { data } = jwt.verify(token, process.env.JWT_SECRET);
